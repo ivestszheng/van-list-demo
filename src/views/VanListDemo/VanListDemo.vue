13e752d7c331f17e58481ffca1b3ae45c862bc01
无声 <ivestszheng@qq.com>
2023-06-08 16:56:01 +0800

    feat: 抽象函数，增加入参

--- src/views/VanListDemo/VanListDemo.vue
+++ src/views/VanListDemo/VanListDemo.vue
@@ -30,18 +30,72 @@
 </template>
 
 <script setup lang="ts">
+import { reactive, ref, toRefs, unref } from "vue";
 import { useVanList } from "./composables/vanList";
 
-const {
-  searchContent,
-  onSearch,
-  list,
-  loading,
-  finished,
-  onLoad,
-  isRefreshing,
-  onRefresh,
-} = useVanList();
+const queryCondition = reactive({
+    page: 0,
+    size: 8,
+    searchContent: "",
+  }),
+  searchContent = ref(""),
+  total = ref(40),
+  MAX_SIZE = 40,
+  { page } = toRefs(queryCondition);
+
+const { list, loading, finished, onLoad, isRefreshing, onRefresh, onSearch } =
+  useVanList({
+    page,
+    total,
+    searchContent,
+    queryContent,
+    beforeSearch,
+  });
+
+function queryContent() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      if (
+        searchContent.value &&
+        Number(searchContent.value) > 0 &&
+        Number(searchContent.value) <= MAX_SIZE
+      ) {
+        const items = [Number(queryCondition.searchContent)];
+        resolve({ clear: true, items });
+      } else if (
+        searchContent.value &&
+        (Number(searchContent.value) <= 0 ||
+          Number(searchContent.value) > MAX_SIZE)
+      ) {
+        resolve({ clear: true, items: [] });
+      } else {
+        const items = [];
+        for (let i = 1; i < queryCondition.size + 1; i++) {
+          items.push(list.value.length + i);
+        }
+        resolve({
+          clear: false,
+          items,
+        });
+      }
+    }, 1000);
+  });
+}
+
+function beforeSearch() {
+  queryCondition.searchContent = unref(searchContent);
+  // total 的赋值只是因为实现显示逻辑，真实业务不需要有此逻辑
+  if (searchContent.value && Number(searchContent.value) <= MAX_SIZE) {
+    total.value = 1;
+  } else if (
+    (searchContent.value && Number(searchContent.value) <= 0) ||
+    Number(searchContent.value) > MAX_SIZE
+  ) {
+    total.value = 0;
+  } else {
+    total.value = MAX_SIZE;
+  }
+}
 </script>
 
 <style scoped>
